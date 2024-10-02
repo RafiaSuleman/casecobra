2c4d390688f10626b68076094bdd62568bdc72bf
RafiaSuleman <rafiasuleman47@gmail.com>
2024-10-02 14:30:30 +0500

    preview error

--- src/app/configure/preview/page.tsx
+++ src/app/configure/preview/page.tsx
@@ -1,12 +1,29 @@
 import { db } from '@/db'
 import { notFound } from 'next/navigation'
 import DesignPreview from './designPreview'
+import { PrismaClient } from '@prisma/client';
+import { any } from 'zod';
+
+declare global {
+  // Add your custom property to globalThis
+  var prisma: PrismaClient | undefined;
+}
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma){
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined
   }
 }
+
 // server side component in which we get id in searchparams
 const Page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams
