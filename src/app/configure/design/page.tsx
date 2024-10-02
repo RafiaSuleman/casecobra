a1419d959b568cc5c01fe414d8866de1cdf69470
RafiaSuleman <rafiasuleman47@gmail.com>
2024-10-02 15:08:47 +0500

    design page error

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,39 +1,42 @@
-import { db } from '@/db'
-import { notFound } from 'next/navigation'
-import DesignConfigurator from './designconfigurator'
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+import DesignConfigurator from './designconfigurator';
 
 interface PageProps {
   searchParams: {
-    [key: string]: string | string[] | undefined
-  }
+    [key: string]: string | string[] | undefined;
+  };
 }
 
 const Page = async ({ searchParams }: PageProps) => {
-  const { id } = searchParams
+  const { id } = searchParams;
 
   if (!id || typeof id !== 'string') {
-    return notFound()
+    return notFound();
   }
 
-  //tell prisama to find the configration where unique id passed
-  const configuration = await db.configuration.findUnique({
-    where: { id },
-  })
+  try {
+    const configuration = await db.configuration.findUnique({
+      where: { id },
+    });
 
-  if (!configuration) {
-    return notFound()
+    if (!configuration) {
+      return notFound();
+    }
+
+    const { imageUrl, width, height } = configuration;
+
+    return (
+      <DesignConfigurator
+        configId={configuration.id}
+        imageDimensions={{ width, height }}
+        imageUrl={imageUrl}
+      />
+    );
+  } catch (error) {
+    console.error('Error fetching configuration:', error);
+    return notFound();
   }
+};
 
-  const { imageUrl, width, height } = configuration
-
-  return (
-   
-     <DesignConfigurator
-     configId={configuration.id}
-      imageDimensions={{ width, height }}
-      imageUrl={imageUrl}
-    />
-  )
-}
-
-export default Page
+export default Page;
